ec47a3b854d8ef3286a534d3bd82e42e0fb0fc74
Daniel Bonrath <bonne@clockwork-origins.de>
2017-02-12 16:32:49 +0100

    #744 treasure hunt is also triggered now when spawning near the pirates in chapter 4

--- _work/data/Scripts/Content/FUNKTIONEN/GILDENSTORY_PIR.d
+++ _work/data/Scripts/Content/FUNKTIONEN/GILDENSTORY_PIR.d
@@ -182,7 +182,6 @@ FUNC VOID GILDENSTORY_PIR ()
 		if (Mod_Piraten_Aufbruch == 0)
 		&& (Npc_KnowsInfo(hero, Info_Mod_Greg_Kompass))
 		&& (Kapitel >= 4)
-		&& (Npc_GetDistToWP(hero, "STRAND") > 10000)
 		&& ((!Npc_KnowsInfo(hero, Info_Mod_Myxir_Amulett))
 		|| (Npc_KnowsInfo(hero, Info_Mod_Greg_AJAlive)))
 		{
