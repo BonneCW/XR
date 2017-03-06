dd1390df6b5f0324f1bed93aed74a6ad0bc4d126
Daniel Bonrath <bonne@clockwork-origins.de>
2017-03-06 19:02:57 +0100

    #773 improved dialog and log entry of Falin

--- _work/data/Scripts/Content/Story/Dialoge/Falin.d
+++ _work/data/Scripts/Content/Story/Dialoge/Falin.d
@@ -17,19 +17,19 @@ FUNC INT Info_Mod_Falin_Hi_Condition()
 FUNC VOID Info_Mod_Falin_Hi_Info()
 {
 	AI_Output(hero, self, "Info_Mod_Falin_Hi_15_00"); //Hallo! Wer bist du und was machst du hier?
-	AI_Output(self, hero, "Info_Mod_Falin_Hi_07_01"); //Ich bin Falin und auf dem Weg ins alte Kloster.
+	AI_Output(self, hero, "Info_Mod_Falin_Hi_07_01"); //Ich bin Falin und auf dem Weg ins alte Kloster der Verwandlungsmagier.
 	AI_Output(hero, self, "Info_Mod_Falin_Hi_15_02"); //Und was lungerst du dann hier rum?
 	AI_Output(self, hero, "Info_Mod_Falin_Hi_07_03"); //War neugierig. Wusste nicht, dass hier ein Lager ist.
 	AI_Output(hero, self, "Info_Mod_Falin_Hi_15_04"); //Dies ist das Waldläuferlager. Der Druide Cyrco ist der Anführer. Wir suchen Nahkämpfer zur Verstärkung.
 	AI_Output(self, hero, "Info_Mod_Falin_Hi_07_05"); //Ehrlich? Das könnte mir gefallen. Immer der selbe Trott bei uns. Wache schieben oder rumsitzen in der Höhle.
 	AI_Output(hero, self, "Info_Mod_Falin_Hi_15_06"); //Da ist hier mehr los. Also willst du?
-	AI_Output(self, hero, "Info_Mod_Falin_Hi_07_07"); //Ich muss erst dieses Paket ins Kloster bringen.
+	AI_Output(self, hero, "Info_Mod_Falin_Hi_07_07"); //Ich muss erst dieses Paket ins Kloster zu Ferco bringen.
 	AI_Output(hero, self, "Info_Mod_Falin_Hi_15_08"); //Weißt du was? Ich bring das rüber. Du gehst ins Lager. Sag der Torwache, du bist ein Neuer. Er wird dich zu Cyrco schicken.
 	AI_Output(self, hero, "Info_Mod_Falin_Hi_07_09"); //Das wäre mir grad recht. Hier, das Paket.
 
 	B_GiveInvItems	(self, hero, ItMi_PaketFalin, 1);
 
-	B_LogEntry	(TOPIC_MOD_JG_NAHKAMPF, "Möglicherweise habe ich schon einen Söldner gefunden, jetzt mal in Khorinis nachfragen. Ach ja. Und das Paket fürs Kloster ...");
+	B_LogEntry	(TOPIC_MOD_JG_NAHKAMPF, "Möglicherweise habe ich schon einen Söldner gefunden, jetzt mal in Khorinis nachfragen. Ach ja. Und das Paket fürs Kloster der Verwandlungsmagier, welches ich bei Ferco abgeben soll ...");
 
 	AI_StopProcessInfos	(self);
 
