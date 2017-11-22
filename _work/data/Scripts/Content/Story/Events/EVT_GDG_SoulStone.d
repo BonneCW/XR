f334bc69b18f230dc9d186a49d6b32669635d6ed
Daniel Bonrath <bonne@clockwork-origins.de>
2017-11-22 18:13:12 +0100

    #1048 removed topic 'Das Ritual' as it is used for just one log entry that belongs to another quest

--- _work/data/Scripts/Content/Story/Events/EVT_GDG_SoulStone.d
+++ _work/data/Scripts/Content/Story/Events/EVT_GDG_SoulStone.d
@@ -8,7 +8,7 @@ FUNC VOID EVT_GDG_SoulStone_s1()
 	{
 		Wld_PlayEffect("FX_EarthQuake",  hero, hero, 0, 0, 0, FALSE );
 
-		B_LogEntry	(TOPIC_MOD_GDG_RITUAL, "Ich habe alle 3 Seelenstein eingesetzt. Beim Einsetzen des letzten Foki gab es ein Erdbeben. Ich kann jetzt Xardas, Pyrokar und Saturas holen und das Ritual kann beginnen.");
+		B_LogEntry	(TOPIC_MOD_URIZIEL, "Ich habe alle 3 Seelenstein eingesetzt. Beim Einsetzen des letzten Foki gab es ein Erdbeben. Ich kann jetzt Xardas, Pyrokar und Saturas holen und das Ritual kann beginnen.");
 
 		B_GivePlayerXP	(1000);
 
