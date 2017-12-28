0838dc9e45a8e27ee8ef0dd8b411fd35e16c484a
Daniel Bonrath <bonne@clockwork-origins.de>
2017-12-28 22:01:01 +0100

    monsters now can't output SVMs anymore

--- _work/data/Scripts/Content/AI/Human/B_Human/B_Say.d
+++ _work/data/Scripts/Content/AI/Human/B_Human/B_Say.d
@@ -4,5 +4,8 @@
 
 func void B_Say (var C_NPC slf, var C_Npc oth, var string text)
 {
+	if (slf.guild > GIL_SEPERATOR_HUM && slf.guild != GIL_DRAGON) {
+		return;
+	};
 	AI_OutputSVM (slf, oth, text);
 };
