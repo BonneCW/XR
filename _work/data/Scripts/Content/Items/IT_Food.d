396cf46792bace2e04f66c74bed58e7330ad82fa
Daniel Bonrath <bonne@clockwork-origins.de>
2017-12-29 12:58:24 +0100

    #1108 Matteo's apples can't be sold anymore

--- _work/data/Scripts/Content/Items/IT_Food.d
+++ _work/data/Scripts/Content/Items/IT_Food.d
@@ -185,7 +185,7 @@ INSTANCE ItFo_Apple_Matteo (C_Item)
 	name 				=	"Apfel";
 
 	mainflag 			=	ITEM_KAT_FOOD;
-	flags 				=	ITEM_MULTI;
+	flags 				=	ITEM_MULTI | ITEM_SHOW;
 	
 	value 				=	Value_Apple;
 	
@@ -204,7 +204,7 @@ INSTANCE ItFo_Apple_Matteo_Gut (C_Item)
 	name 				=	"knackiger Apfel";
 
 	mainflag 			=	ITEM_KAT_FOOD;
-	flags 				=	ITEM_MULTI;
+	flags 				=	ITEM_MULTI | ITEM_SHOW;
 	
 	value 				=	Value_Apple;
 	
@@ -223,7 +223,7 @@ INSTANCE ItFo_Apple_Matteo_Schlecht (C_Item)
 	name 				=	"fauler Apfel";
 
 	mainflag 			=	ITEM_KAT_FOOD;
-	flags 				=	ITEM_MULTI;
+	flags 				=	ITEM_MULTI | ITEM_SHOW;
 	
 	value 				=	Value_Apple;
 	
