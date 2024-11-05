995c5090c1bc6f4daa4df6fb073b943e96b5a789
Daniel Bonrath <bonne@clockwork-origins.de>
2024-11-05 20:23:17 +0100

    fixed key for storage in old camp cellar missing

--- _work/data/Scripts/Content/Items/Neue_Items/New_Keys.d
+++ _work/data/Scripts/Content/Items/Neue_Items/New_Keys.d
@@ -30,3 +30,21 @@ INSTANCE ItKe_Mod_Gunnar_Dieb(C_Item)
 	Text[0]				=	"Diesen Schlüssel hatte Gunnar bei sich";
 	TEXT[5]				= 	NAME_Value;	COUNT[5]	= value;
 };
+
+INSTANCE ITKE_STORAGE_01(C_Item)
+{
+	name 				=	"Schlüssel";
+
+	mainflag 			=	ITEM_KAT_NONE;
+	flags 				=	0;
+
+	value 				=	Value_Key_01;
+
+	visual 				=	"ItKe_Key_01.3ds";
+	material 			=	MAT_METAL;
+
+	description			= 	name;
+	Text[0]				=	"Dieser Schlüssel passt zum Keller";
+	Text[1]				=	"im Burghof des Alten Lagers";
+	TEXT[5]				= 	NAME_Value;	COUNT[5]	= value;
+};
