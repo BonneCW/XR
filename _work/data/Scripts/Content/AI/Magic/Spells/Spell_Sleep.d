0df2a0ad7789c511f5339aa403d49d1ac329ba4b
Daniel Bonrath <bonne@clockwork-origins.de>
2024-11-19 20:34:08 +0100

    fixed sleep spell not working on Naim

--- _work/data/Scripts/Content/AI/Magic/Spells/Spell_Sleep.d
+++ _work/data/Scripts/Content/AI/Magic/Spells/Spell_Sleep.d
@@ -2,7 +2,7 @@
 // SPL_Sleep
 // *********
 
-const int SPL_Cost_Sleep				= 30;	
+const int SPL_Cost_Sleep				= 30;
 const int SPL_TIME_Sleep				= 30;	// in ZS_MagicSleep
 
 
@@ -26,20 +26,21 @@ func int Spell_Logic_Sleep (var int manaInvested) //Parameter manaInvested wird 
 		{
 			self.attribute[ATR_MANA] = self.attribute[ATR_MANA] - SPL_Cost_Sleep;
 		};
-		
+
 		if (!C_BodyStateContains(other, BS_SWIM))
 		&& (!C_BodyStateContains(other, BS_DIVE))
 		&& (!C_NpcIsDown(other))
 		&& (other.guild < GIL_SEPERATOR_HUM)
 		&& ((other.flags != NPC_FLAG_IMMORTAL)
+		|| (Hlp_GetInstanceID(other) == Hlp_GetInstanceID(Mod_7147_ASS_Naim_NW))
 		|| (Hlp_GetInstanceID(other) == Hlp_GetInstanceID(Mod_7358_DMR_Daemonenritter_MT)))
 		&& (Npc_GetDistToNpc (self,other) <= 1000)
-		{		
+		{
 			Npc_ClearAIQueue	(other);
 			B_ClearPerceptions	(other);
 			AI_StartState		(other, ZS_MagicSleep, 0, "");
 		};
-	
+
 		return SPL_SENDCAST; //Spell wird auch gecasted, wenn keine Auswirkungen (other geht nicht in ZS) Mana is dann weg - Pech gehabt! (soll so sein!)
 	}
 	else //nicht genug Mana
@@ -52,7 +53,7 @@ func int Spell_Logic_Sleep (var int manaInvested) //Parameter manaInvested wird 
 func void Spell_Cast_Sleep()
 {
 	//self.attribute[ATR_MANA] = self.attribute[ATR_MANA] - SPL_Cost_Sleep;			// nicht drin, wegen Kommentar oben
-	
+
 	self.aivar[AIV_SelectSpell] += 1;
 };
 
