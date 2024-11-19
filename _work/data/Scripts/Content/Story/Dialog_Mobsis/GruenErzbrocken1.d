12d5a753a0ce3e319f3dd7d6773113025b369f28
Daniel Bonrath <bonne@clockwork-origins.de>
2024-11-19 20:50:57 +0100

    fixed green ore requiring two types of pick axe

--- _work/data/Scripts/Content/Story/Dialog_Mobsis/GruenErzbrocken1.d
+++ _work/data/Scripts/Content/Story/Dialog_Mobsis/GruenErzbrocken1.d
@@ -1,10 +1,10 @@
 FUNC VOID GruenErzbrocken1_S1 ()
 {
-	var C_NPC her; 	her = Hlp_GetNpc(PC_Hero); 
-	
+	var C_NPC her; 	her = Hlp_GetNpc(PC_Hero);
+
 	if  (Hlp_GetInstanceID(self)==Hlp_GetInstanceID(her))
-	{	
-		if (Npc_HasItems(hero, ItMw_2H_Axe_L_01) == 0)
+	{
+		if (Npc_HasItems(hero, ItMw_Stahlhacke) == 0)
 		{
 			Print	("Ohne Spitzhacke geht das nicht!");
 
@@ -25,20 +25,20 @@ INSTANCE PC_GruenErzbrocken1_Addon_Hour (C_Info)
 	condition		= PC_GruenErzbrocken1_Addon_Hour_Condition;
 	information		= PC_GruenErzbrocken1_Addon_Hour_Info;
 	permanent		= 0;
-	description		= "Einfach mal hacken. "; 
+	description		= "Einfach mal hacken. ";
 };
 
 FUNC INT PC_GruenErzbrocken1_Addon_Hour_Condition ()
 {
 	if (PLAYER_MOBSI_PRODUCTION	==	MOBSI_GruenErzbrocken1)
-	{	
+	{
 		return TRUE;
 	};
 };
 
 FUNC VOID PC_GruenErzbrocken1_Addon_Hour_Info()
 {
-	CreateInvItems (hero, ItMi_GreenNugget, 2);	
+	CreateInvItems (hero, ItMi_GreenNugget, 2);
 
 	PrintScreen ("2 grüne Erzbrocken gehackt!", -1, -1, FONT_ScreenSmall, 2);
 };
@@ -50,13 +50,13 @@ INSTANCE PC_GruenErzbrocken1_End (C_Info)
 	condition		= PC_GruenErzbrocken1_End_Condition;
 	information		= PC_GruenErzbrocken1_End_Info;
 	permanent		= TRUE;
-	description		= DIALOG_ENDE; 
+	description		= DIALOG_ENDE;
 };
 
 FUNC INT PC_GruenErzbrocken1_End_Condition ()
 {
 	if (PLAYER_MOBSI_PRODUCTION	==	MOBSI_GruenErzbrocken1)
-	{	
+	{
 		return TRUE;
 	};
 };
--- _work/data/Scripts/Content/Story/Dialog_Mobsis/GruenErzbrocken12.d
+++ _work/data/Scripts/Content/Story/Dialog_Mobsis/GruenErzbrocken12.d
@@ -1,10 +1,10 @@
 FUNC VOID GruenErzbrocken12_S1 ()
 {
-	var C_NPC her; 	her = Hlp_GetNpc(PC_Hero); 
-	
+	var C_NPC her; 	her = Hlp_GetNpc(PC_Hero);
+
 	if  (Hlp_GetInstanceID(self)==Hlp_GetInstanceID(her))
-	{	
-		if (Npc_HasItems(hero, ItMw_2H_Axe_L_01) == 0)
+	{
+		if (Npc_HasItems(hero, ItMw_Stahlhacke) == 0)
 		{
 			Print	("Ohne Spitzhacke geht das nicht!");
 
@@ -25,20 +25,20 @@ INSTANCE PC_GruenErzbrocken12_Addon_Hour (C_Info)
 	condition		= PC_GruenErzbrocken12_Addon_Hour_Condition;
 	information		= PC_GruenErzbrocken12_Addon_Hour_Info;
 	permanent		= 0;
-	description		= "Einfach mal hacken. "; 
+	description		= "Einfach mal hacken. ";
 };
 
 FUNC INT PC_GruenErzbrocken12_Addon_Hour_Condition ()
 {
 	if (PLAYER_MOBSI_PRODUCTION	==	MOBSI_GruenErzbrocken12)
-	{	
+	{
 		return TRUE;
 	};
 };
 
 FUNC VOID PC_GruenErzbrocken12_Addon_Hour_Info()
 {
-	CreateInvItems (hero, ItMi_GreenNugget, 6);	
+	CreateInvItems (hero, ItMi_GreenNugget, 6);
 
 	PrintScreen ("6 grüne Erzbrocken gehackt!", -1, -1, FONT_ScreenSmall, 2);
 };
@@ -50,13 +50,13 @@ INSTANCE PC_GruenErzbrocken12_End (C_Info)
 	condition		= PC_GruenErzbrocken12_End_Condition;
 	information		= PC_GruenErzbrocken12_End_Info;
 	permanent		= TRUE;
-	description		= DIALOG_ENDE; 
+	description		= DIALOG_ENDE;
 };
 
 FUNC INT PC_GruenErzbrocken12_End_Condition ()
 {
 	if (PLAYER_MOBSI_PRODUCTION	==	MOBSI_GruenErzbrocken12)
-	{	
+	{
 		return TRUE;
 	};
 };
